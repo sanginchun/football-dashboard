9150809588913b47f5e10c21f5c11ad8975122fd
sanginchun <sanginchun91@gmail.com>
2021-03-07 15:55:30 +0900

    initial setting done

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import App from "./src/App.js";
 
 new App(document.querySelector("#app"));
--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 class App {
   constructor($target) {
     const h1 = document.createElement("h1");
     h1.innerHTML = "app running";
     $target.appendChild(h1);
+
+    this.someFunc();
+  }
+
+  async someFunc() {
+    try {
+      const a = await fetch("ll");
+
+      console.log(a);
+    } catch (err) {
+      console.log(error);
+    }
   }
 }
 
